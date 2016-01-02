0f5ddc6dee34feccc24e9a541ef01fb27d5552df
jincheng9 <perfume0607@gmail.com>
2016-01-02 16:44:23 +0800

    [feature]login

--- solution/CTPTest/CTPTest/TraderSpi.cpp
+++ solution/CTPTest/CTPTest/TraderSpi.cpp
@@ -13,6 +13,24 @@ MyTraderSpi::~MyTraderSpi()
 void MyTraderSpi::OnFrontConnected()
 {
 	cout << "connected" << endl;
+	// login
+	CThostFtdcReqUserLoginField reqUserLogin;
+	TThostFtdcBrokerIDType brokerId;
+	cout << "input broker id:";
+	cin >> brokerId;
+	strcpy(reqUserLogin.BrokerID, brokerId);
+
+	TThostFtdcUserIDType userId;
+	cout << "input user id:";
+	cin >> userId;
+	strcpy(reqUserLogin.UserID, userId);
+
+	TThostFtdcPasswordType pwd;
+	cout << "input password:";
+	cin >> pwd;
+	strcpy(reqUserLogin.Password, pwd);
+
+	m_ptrTraderApi->ReqUserLogin(&reqUserLogin, 0);
 }
 
 void MyTraderSpi::OnFrontDisConnected(int nReason)
